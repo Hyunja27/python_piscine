dff75fd2aaba9be2cd096188087fefc2fa0ab7a4
Hyunja27 <123gk@naver.com>
2021-05-25 10:12:21 +0900

    05/25_day02_start

--- day02/ex00/render.py
+++ day02/ex00/render.py
@@ -6,16 +6,18 @@
 #    By: spark <spark@student.42seoul.kr>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/05/25 10:00:07 by spark             #+#    #+#              #
-#    Updated: 2021/05/25 10:03:06 by spark            ###   ########.fr        #
+#    Updated: 2021/05/25 10:11:50 by spark            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 import re
 import os
+from settings import *
 
 def main():
-	print
+	# fd = open()
+	print(name)
 
 if __name__=='__main__':
 	main()
