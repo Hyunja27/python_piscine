96d9397dfbd605a0e787fe1840c97c9366f14fc9
Hyunja27 <123gk@naver.com>
2021-05-26 19:22:11 +0900

    day02_ex06_doing

---
+++ day02/ex06/elem.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+class Text(str):
+    def __str__(self):
+        return super().__str__().replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;').replace('\n', '\n<br />\n')
+
+
+class Elem:
+    class ValidationError(Exception):
+        def __init__(self):
+            super().__init__("Elem has Exception!")
+
+    def __init__(self, tag='div', attr={}, content=None, tag_type='double'):
+        self.tag = tag
+        self.attr = attr
+        if not (Elem.check_type(content) or content is None):
+            raise Elem.ValidationError
+        self.content = []
+        if type(content) == list:
+            self.content = content
+        elif content is not None:
+            self.content.append(content)
+        self.tag_type = tag_type
+
+    def __str__(self):
+        temp1 = ("<{tag}{attr}>{content}</{tag}>")
+        temp2 = ("<{tag}{attr}/>")
+        rt = ""
+        if self.tag_type == 'double':
+            rt = self.__make_content()
+            return temp1.format(tag=self.tag, attr=self.__make_attr(), content=rt)
+        elif self.tag_type == 'simple':
+            return temp2.format(tag=self.tag, attr=self.__make_attr())
+
+    def __make_attr(self):
+        result = ''
+        for pair in sorted(self.attr.items()):
+            result += ' ' + str(pair[0]) + '="' + str(pair[1]) + '"'
+        return result
+
+    def __make_content(self):
+        if len(self.content) == 0:
+            return ''
+        result = "\n"
+        for elem in self.content:
+            if (len(str(elem)) != 0):
+                result += "{elem}\n".format(elem=elem)
+        result = "  ".join(result.splitlines(True))
+        if len(result.strip()) == 0:
+            return ''
+        return result
+
+    def add_content(self, content):
+        if not Elem.check_type(content):
+            raise Elem.ValidationError
+        if type(content) == list:
+            self.content += [elem for elem in content if elem != Text('')]
+        elif content != Text(''):
+            self.content.append(content)
+
+    @staticmethod
+    def check_type(content):
+        return (isinstance(content, Elem) or type(content) == Text or
+                (type(content) == list and all([type(elem) == Text or
+                                                isinstance(elem, Elem)
+                                                for elem in content])))
+
+if __name__ == '__main__':
+    target = Elem('html', content=[
+            Elem('head', content=
+            Elem('title', content=
+            Text('"Hello ground!"'))), 
+            Elem('body', content=[Elem('h1', content=Text('"Oh no, not again!"')), Elem('img', {'src':'http://i.imgur.com/pfp3T.jpg'}, tag_type='simple')])])
+    print(target)
---
+++ day02/ex06/page.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from elements import Elem, Html, Head, Body, Title, Meta, Img, Table, Th, Tr, Td, Ul, Ol, Li, H1, H2, P, Div, Span, Hr, Br
+
+
+class Page:
+    def __init__(self, elem) -> None:
+        if not isinstance(elem, Elem) or type(elem) == Elem:
+            raise Elem.ValidationError()
+        self.elem = elem
+
+    def is_valid(self):
+        if self.check_all(self.elem) == True:
+            return False
+        print("right!")
+        return True
+
+    def check_all(self, elem):
+        if not isinstance(elem, (Html, Head, Body, Title, Meta, Img, Table, Th,
+                     Tr, Td, Ul, Ol, Li, H1, H2, P, Div, Span, Hr, Br)):
+            return True
+        if isinstance(elem, Html) == True:
+            if not (len(elem.content) == 2 and isinstance(elem.content[0], Head) and isinstance(elem.content[1], Body)):
+                return True
+            else:
+                for i in elem.content:
+                    if self.check_all(i) == True:
+                        return True
+        if isinstance(elem, Html) == True:
+
+
+    def write_to_file(self, name: str):
+        fd = open(str, 'r')
+
+
+def main():
+    p = Page(Html())
+    print(p.is_valid())
+
+
+if __name__ == '__main__':
+    main()
