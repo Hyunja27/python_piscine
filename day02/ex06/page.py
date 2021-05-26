fc0a626b53fb1a086337552ec9fb9fabe4defccf
Hyunja27 <123gk@naver.com>
2021-05-26 23:58:11 +0900

    day02_ex06_almost

--- day02/ex06/page.py
+++ day02/ex06/page.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from elements import Elem, Html, Head, Body, Title, Meta, Img, Table, Th, Tr, Td, Ul, Ol, Li, H1, H2, P, Div, Span, Hr, Br
-
+from elem import Text
 
 class Page:
     def __init__(self, elem) -> None:
@@ -13,6 +13,10 @@ class Page:
         if self.check_all(self.elem) == True:
             return False
         return True
+
+    def __str__(self):
+        rt = str(Elem(self.elem))
+        return rt
 
     def check_all(self, elem):
         if not isinstance(elem, (Html, Head, Body, Title, Meta, Img, Table, Th,
@@ -39,7 +43,59 @@ class Page:
                     if self.check_all(k) == True:
                         return True
         
-        # if isinstance(elem, Head) == True:
+        if isinstance(elem, (Body, Div)) == True:
+            for i in elem.content:
+                if isinstance(i, (H1, H2, Div, Table, Ul, Ol, Span, Text)) == False:
+                    return True
+                if all([self.check_all(k) for k in elem.content]):
+                    return True
+        
+        if isinstance(elem, (Title, H1, H2, Li, Th, Td)) == True:
+            ok = -1
+            for i in elem.content:
+                if isinstance(i, Text) == True:
+                    ok += 1
+                if ok > 0:
+                    return True
+            for k in elem.content:
+                    if self.check_all(k) == True:
+                        return True
+
+        if isinstance(elem, P) == True:
+            for i in elem.content:
+                if isinstance(i, Text) == False:
+                    return True
+        
+        if isinstance(elem, Span) == True:
+            ok = -1
+            for i in elem.content:
+                if isinstance(i, (Text, P)) == False:
+                    return True
+        
+        if isinstance(elem, (Ul, Ol)) == True:
+            if len(elem.content) == 0:
+                return True
+            for i in elem.content:
+                if isinstance(i, Li) == False:
+                    return True
+        
+        if isinstance(elem, Tr) == True:
+            th = 0
+            td = 0
+            for i in elem.content:
+                if isinstance(i, Th) == True:
+                    if td == 1:
+                        return True
+                    th = 1
+                if isinstance(i, Td) == True:
+                    if th == 1:
+                        return True
+                    td = 1
+        
+        if isinstance(elem, Table) == True:
+            for i in elem.content:
+                if isinstance(i, Tr) == False:
+                    return True
         
 
     def write_to_file(self, name: str):
@@ -47,9 +103,8 @@ class Page:
 
 
 def main():
-    p = Page(Html([Head(Title(), Title()), Body()]))
-
-    print(p.is_valid())
+    p = Page(Html([Head(Title(Ol(Li()))), Body(Table(Li()))]))
+    print(p)
 
 if __name__ == '__main__':
     main()
