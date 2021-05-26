67660f1d74481f558e3dd7acd30349427252203e
Hyunja27 <123gk@naver.com>
2021-05-26 21:44:57 +0900

    day02_ex06_doing

--- day02/ex06/page.py
+++ day02/ex06/page.py
@@ -12,31 +12,44 @@ class Page:
     def is_valid(self):
         if self.check_all(self.elem) == True:
             return False
-        print("right!")
         return True
 
     def check_all(self, elem):
         if not isinstance(elem, (Html, Head, Body, Title, Meta, Img, Table, Th,
                      Tr, Td, Ul, Ol, Li, H1, H2, P, Div, Span, Hr, Br)):
             return True
+
         if isinstance(elem, Html) == True:
-            if not (len(elem.content) == 2 and isinstance(elem.content[0], Head) and isinstance(elem.content[1], Body)):
+            if not (len(elem.content) == 2 and isinstance(elem.content[0], Head) and
+                                         isinstance(elem.content[1], Body)):
                 return True
             else:
                 for i in elem.content:
                     if self.check_all(i) == True:
                         return True
-        if isinstance(elem, Html) == True:
-
+        
+        if isinstance(elem, Head) == True:
+            ok = -1
+            for i in elem.content:
+                if isinstance(i, Title) == True:
+                    ok += 1
+                if ok > 0:
+                    return True
+            for k in elem.content:
+                    if self.check_all(k) == True:
+                        return True
+        
+        # if isinstance(elem, Head) == True:
+        
 
     def write_to_file(self, name: str):
         fd = open(str, 'r')
 
 
 def main():
-    p = Page(Html())
-    print(p.is_valid())
+    p = Page(Html([Head(Title(), Title()), Body()]))
 
+    print(p.is_valid())
 
 if __name__ == '__main__':
     main()
