7465045cd6eeecda48a846a4e025041b3a7144e7
Hyunja27 <123gk@naver.com>
2021-05-27 05:14:14 +0900

    day03_start

--- day03/ex01/my_script.sh
+++ day03/ex01/my_script.sh
@@ -4,5 +4,7 @@ GIT_URL="https://github.com/jaraco/path"
 PY="/usr/bin/python3"
 LOG_FILE="log_file.log"
 
-$PY -m venv local_lib
+$PY -m venv local_lib | tee $LOG_FILE
 source ./local_lib/bin/activate
+$py pip --version
+$py pip install git+$GIT_URL | tee -a $LOG_FILE
