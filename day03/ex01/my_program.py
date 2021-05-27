18c51c6548bea788830239744adab70700b7b22c
Hyunja27 <123gk@naver.com>
2021-05-27 13:14:22 +0900

    day03_start

--- day03/ex01/my_program.py
+++ day03/ex01/my_program.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
 
+from path import Path
+
+
+def main():
+
+    try:
+        Path.mkdir("spark_dir")
+    except FileExistsError as e:
+        print(e)
+    Path.touch("spark_dir/spark_note")
+    f = Path("spark_dir/spark_note")
+    f.write_lines(["Please Help me.....\nPlease Help me.....\n"
+                   "Please Help me.....\nPlease Help me.....\n",
+                   "Please Help me.....\nPlease Help me.....\n",
+                   "Please Help me.....\nPlease Help me.....\n",
+                   "Please Help me.....\nPlease Help me.....\n",
+                   "Please Help me.....\nPlease Help me.....\n"])
+    print(f.read_text())
+
+
+if __name__ == '__main__':
+    main()
--- day03/ex01/my_script.sh
+++ day03/ex01/my_script.sh
@@ -5,6 +5,12 @@ PY="/usr/bin/python3"
 LOG_FILE="log_file.log"
 
 $PY -m venv local_lib | tee $LOG_FILE
+
 source ./local_lib/bin/activate
-$py pip --version
-$py pip install git+$GIT_URL | tee -a $LOG_FILE
+
+echo "\n\n== Welcome! ==\n\n"
+python -m pip --version | tee -a $LOG_FILE
+echo "\n\n"
+python -m pip install --force-reinstall git+$GIT_URL | tee -a $LOG_FILE
+
+python my_program.py
