cc2c33b4b41c010ff01c2582cfc3dce36260e0f8
Hyunja27 <123gk@naver.com>
2021-05-27 21:00:31 +0900

    day03_ex04start

--- day03/ex03/roads_to_philosophy.py
+++ day03/ex03/roads_to_philosophy.py
@@ -3,10 +3,29 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
+from requests.api import get
+
+
+def road_to_phil(URL: str):
+	rt = requests.get(url=URL)
+	source = BeautifulSoup(rt.text, 'html.parser')
+	rt = source.find(id="mw-content-text").find_all('a')
+
+	print(rt)
+
+	# val = str(rt[0]).split('"')
+	# print(val[1])
+	# url = 'https://en.wikipedia.org{page}'.format(page=val[1])
+	# road_to_phil(url)
+
 
 def main():
-	print()
+	if len(sys.argv) != 2:
+		raise Exception("Error : single arg needed")
+	arg = "/wiki/" + sys.argv[1]
+	URL = 'https://en.wikipedia.org{page}'.format(page=arg)
+	road_to_phil(URL)
 
 
-if __name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()
