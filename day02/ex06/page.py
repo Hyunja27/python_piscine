61db57ed08744924a972c408119019a62d3b5684
Hyunja27 <123gk@naver.com>
2021-05-28 23:24:23 +0900

    day04_start

--- day02/ex06/page.py
+++ day02/ex06/page.py
@@ -3,6 +3,7 @@
 from elements import Elem, Html, Head, Body, Title, Meta, Img, Table, Th, Tr, Td, Ul, Ol, Li, H1, H2, P, Div, Span, Hr, Br
 from elem import Text
 
+
 class Page:
     def __init__(self, elem) -> None:
         if not isinstance(elem, Elem) or type(elem) == Elem:
@@ -17,7 +18,7 @@ class Page:
     def __str__(self):
         rt = str(self.elem)
         if not isinstance(self.elem, Html):
-             return rt
+            return rt
         return "<!DOCTYPE html>\n" + rt
 
     def write_to_file(self, name: str):
@@ -29,21 +30,20 @@ class Page:
             fd.write(rt)
         fd.close()
 
-
     def check_all(self, elem):
         if not isinstance(elem, (Html, Head, Body, Title, Meta, Img, Table, Th,
-                     Tr, Td, Ul, Ol, Li, H1, H2, P, Div, Span, Hr, Br)):
+                                 Tr, Td, Ul, Ol, Li, H1, H2, P, Div, Span, Hr, Br)):
             return True
 
         if isinstance(elem, Html) == True:
             if not (len(elem.content) == 2 and isinstance(elem.content[0], Head) and
-                                         isinstance(elem.content[1], Body)):
+                    isinstance(elem.content[1], Body)):
                 return True
             else:
                 for i in elem.content:
                     if self.check_all(i) == True:
                         return True
-        
+
         if isinstance(elem, Head) == True:
             ok = -1
             for i in elem.content:
@@ -52,45 +52,47 @@ class Page:
                 if ok > 0:
                     return True
             for k in elem.content:
-                    if self.check_all(k) == True:
-                        return True
-        
+                if self.check_all(k) == True:
+                    return True
+
         if isinstance(elem, (Body, Div)) == True:
             for i in elem.content:
                 if isinstance(i, (H1, H2, Div, Table, Ul, Ol, Span, Text)) == False:
                     return True
                 if all([self.check_all(k) for k in elem.content]):
                     return True
-        
+
         if isinstance(elem, (Title, H1, H2, Li, Th, Td)) == True:
             ok = -1
             for i in elem.content:
                 if isinstance(i, Text) == True:
                     ok += 1
+                else:
+                    return True
                 if ok > 0:
                     return True
-            for k in elem.content:
-                    if self.check_all(k) == True:
-                        return True
+            # for k in elem.content:
+            #         if self.check_all(k) == True:
+            #             return True
 
         if isinstance(elem, P) == True:
             for i in elem.content:
                 if isinstance(i, Text) == False:
                     return True
-        
+
         if isinstance(elem, Span) == True:
             ok = -1
             for i in elem.content:
                 if isinstance(i, (Text, P)) == False:
                     return True
-        
+
         if isinstance(elem, (Ul, Ol)) == True:
             if len(elem.content) == 0:
                 return True
             for i in elem.content:
                 if isinstance(i, Li) == False:
                     return True
-        
+
         if isinstance(elem, Tr) == True:
             th = 0
             td = 0
@@ -103,7 +105,7 @@ class Page:
                     if th == 1:
                         return True
                     td = 1
-        
+
         if isinstance(elem, Table) == True:
             for i in elem.content:
                 if isinstance(i, Tr) == False:
@@ -127,14 +129,18 @@ def tester():
             print("Bye!")
             return
         p = Page(eval(t))
-        print(p.is_valid)
+        print(p.is_valid())
         print("\n\n------------------------------------\n")
-        # print("\n\nstd print is =>\n\n", p)
         print("\n\n------------------------------------\n")
         print("\n====> Enter your Test line!  :)\n")
 
+
 def main():
     tester()
+    # p = Page(H1(Text("HEllo")))
+    # print(p)
+    # print(p.is_valid())
+
 
 if __name__ == '__main__':
     main()
