22e7cdea97f1456408df20b10822914c3d1f2e5a
Hyunja27 <123gk@naver.com>
2021-05-29 18:00:02 +0900

    day04_02_doing

--- day04/ex02/urls.py
+++ day04/ex02/urls.py
@@ -1,8 +1,9 @@
+from django.conf.urls import url
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-	path('input', views.show_page_input, name='show_page_input'),
-	path('log', views.show_page_log, name='show_page_log')
+	path('input', views.control_page, name='control_page'),
+	path('log', views.show_page_log, name='show_page_log'),
+	url('thanks', views.thanks),
 ]
--- day04/ex02/views.py
+++ day04/ex02/views.py
@@ -2,25 +2,33 @@ from django.shortcuts import render
 from django import forms
 from .forms import MsForm
 
-def show_page_input(request):
-	return render(request, "input.html")
+# def show_page_input(request):
+# 	return render(request, "input.html")
 
 def show_page_log(request):
 	return render(request, "log.html")
 
-# def control_page(request):
-# 	if request.method == 'POST':
-# 		# create a form instance and populate it with data from the request:
-# 		form =  MsForm(request.POST)
+def control_page(request):
+	if request.method == 'POST':
+		# create a form instance and populate it with data from the request:
+		form =  MsForm(request.POST)
 
-# 		# check whether it's valid:
-# 		if form.is_valid():
-# 			forms.your_ms <= 확인된 데이터
-# 			# process the data in form.cleaned_data as required
-# 			# ...
-# 			# redirect to a new URL:
-# 			return HttpResponseRedirect('/myform/thanks/')
-# 	return render(request, "input.html")
+		
+		print(forms.cleaned_data[form])
+		print(form)
+		print('!!message =', form)
+		# check whether it's valid:
+		if form.is_valid():
+			vaild_ms = forms.cleaned_data['your_ms']
+			print('message =', vaild_ms)
+			# process the data in form.cleaned_data as required
+			# ...
+			# redirect to a new URL:
+			return HttpResponseRedirect('thanks/')
+	else:
+		form = MsForm()
+		context = {'form': form}
+	return render(request, "input.html", context)
 
 def thanks(request):
-    return render(request, 'myform/thanks.html')
+    return render(request, 'thanks.html')
