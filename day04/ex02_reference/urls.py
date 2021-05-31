ee77813c6fc2e3905cc85fc3ccdaec16dd971f3a
Hyunja27 <123gk@naver.com>
2021-05-31 20:45:03 +0900

    conflict_fixed

--- day04/ex02_reference/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('input', views.control_page, name='control_page'),
-	path('log', views.show_page_log, name='show_page_log'),
-	url('thanks', views.thanks),
-]
